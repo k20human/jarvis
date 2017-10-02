cb9c3524e01dac53164c5b28f09bc4ef2465c6d7
Kévin Mathieu <kevin.mathieu@insidegroup.fr>
2017-10-02 19:24:02 +0200

    Improve Angular base

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,10 +1,10 @@
-import { JarvisPage } from './app.po';
+import { AppPage } from './app.po';
 
 describe('jarvis App', () => {
-  let page: JarvisPage;
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new JarvisPage();
+    page = new AppPage();
   });
 
   it('should display welcome message', () => {
